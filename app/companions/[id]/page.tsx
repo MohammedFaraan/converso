9a8d8616e6f68c19633b886419935b0f98ed2c44
MohammedFaraan <mohammedfaran2006@gmail.com>
2025-08-23 23:33:14 +0530

    Add Vapi API Key & Implement Get Companion Server action

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,4 +1,23 @@
-const CompanionSession =() => {
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface CompanionSessionProps {
+  params : Promise<{id: string}>
+}
+
+const CompanionSession = async ({params} : CompanionSessionProps ) => {
+  const {id} = await params;
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+  console.log(user, id);
+  const companion = await getCompanion(id);
+
+  if (!companion.name) redirect("/companions");
+
+  console.log(companion);
+
   return (
     <div>
      CompanionSession 
