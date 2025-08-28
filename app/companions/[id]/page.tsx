a9c86c9b8d434cd6595d581cacad61f22553d614
MohammedFaraan <mohammedfaran2006@gmail.com>
2025-08-28 23:10:35 +0530

    Implement Companion Session

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,28 +1,65 @@
+import CompanionSection from "@/components/CompanionSection";
 import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 interface CompanionSessionProps {
-  params : Promise<{id: string}>
+  params: Promise<{ id: string }>;
 }
 
-const CompanionSession = async ({params} : CompanionSessionProps ) => {
-  const {id} = await params;
+const CompanionSession = async ({ params }: CompanionSessionProps) => {
+  const { id } = await params;
   const user = await currentUser();
 
   if (!user) redirect("/sign-in");
-  console.log(user, id);
-  const companion = await getCompanion(id);
 
-  if (!companion.name) redirect("/companions");
+  const companion = await getCompanion(
+    id
+  );
 
-  console.log(companion);
+  const { name, topic, subject, style, voice, duration } = companion;
+
+  if (!name) redirect("/companions");
+  
+  // console.log(companion);
 
   return (
-    <div>
-     CompanionSession 
-    </div>
-  )
-}
+    <main>
+      <article className="flex rounded-border p-6 justify-between max-md:flex-col">
+        <div className="flex items-center gap-4">
+          <div
+            className="size-[72px] flex justify-center items-center max-md:hidden rounded-lg"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt={subject}
+              height={35}
+              width={35}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+            <p className="font-bold text-2xl">{name}</p>
+            <div className="subject-badge h-fit">{subject}</div>
+          </div>
+          <p>{topic}</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-xl max-sm:hidden">{duration} minutes</p>
+        </div>
+      </article>
+      <CompanionSection 
+      {...companion}
+      companionId={id}
+      userName={user.firstName!}
+      userImage={user.imageUrl!}
+      />
+    </main>
+  );
+};
 
-export default CompanionSession
+export default CompanionSession;
---
+++ components/CompanionSection.tsx
@@ -0,0 +1,156 @@
+"use client";
+
+import { cn, getSubjectColor } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import soundwaves from "@/constants/soundwaves.json";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
+
+const CompanionSection = ({
+  name,
+  topic,
+  subject,
+  style,
+  voice,
+  companionId,
+  userName,
+  userImage,
+}: CompanionComponentProps) => {
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+
+  const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+  useEffect(() => {
+    if (lottieRef) {
+      if (isSpeaking) {
+        lottieRef.current?.play();
+      } else {
+        lottieRef.current?.stop();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+
+    const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+
+    const onMessage = () => {};
+
+    const onError = (error: Error) => console.log("Error", error);
+
+    const onSpeechStart = () => setIsSpeaking(true);
+
+    const onSpeechEnd = () => setIsSpeaking(false);
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("error", onError);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("error", onError);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+    };
+  }, []);
+
+  const toggleMicrophone = () => {
+    const isMuted = vapi.isMuted;
+    vapi.setMuted(!isMuted);
+    setIsMuted(!isMuted);
+  };
+
+  return (
+    <section className="flex flex-col h-[70vh]">
+      <section className="flex gap-8 max-sm:flex-col">
+        <div className="companion-section p-4">
+          <div
+            className="companion-avatar"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <div
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                callStatus === CallStatus.FINISHED ||
+                  callStatus === CallStatus.INACTIVE
+                  ? "opacity-1001"
+                  : "opacity-0",
+                callStatus === CallStatus.CONNECTING &&
+                  "opacity-100 animate-pulse"
+              )}
+            >
+              <Image
+                src={`/icons/${subject}.svg`}
+                alt={subject}
+                height={150}
+                width={150}
+                className="max-sm:w-fit"
+              />
+            </div>
+
+            <div
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                callStatus === CallStatus.ACTIVE ? "opacity-100" : "opacity-0"
+              )}
+            >
+              <Lottie
+                lottieRef={lottieRef}
+                animationData={soundwaves}
+                autoplay={false}
+                className="companion-lottie"
+              />
+            </div>
+          </div>
+          <p className="font-bold text-2xl">{topic}</p>
+        </div>
+        <div className="user-section">
+          <div className="user-avatar">
+            <Image
+              src={userImage}
+              alt={userImage}
+              height={130}
+              width={130}
+              className="rounded-lg"
+            />
+            <p className="font-bold text-2xl">{userName}</p>
+          </div>
+          <button
+            className="btn-mic"
+            onClick={toggleMicrophone}
+            disabled={callStatus === CallStatus.ACTIVE}
+          >
+            <Image
+              src={isMuted ? "/icons/mic-off.svg" : "/icons/mic-on.svg"}
+              alt="mic"
+              height={36}
+              width={36}
+              className="rounded-lg"
+            />
+            <p className="max-sm:hidden">
+              {isMuted ? "Turn on microphone" : "Turn off microphone"}
+            </p>
+          </button>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CompanionSection;
